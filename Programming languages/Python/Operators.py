fbcffc990c2716b4a6543b3ab16c90446cf40e28
ROkelbow <kelbowow@gmail.com>
2024-02-14 21:44:02 +0330

    Create Operators.py

---
+++ Programming languages/Python/Operators.py
@@ -0,0 +1,45 @@
+# Operators Python Script
+
+# Arithmetic Operators
+# Addition
+result_addition = 5 + 3
+print("Addition Result:", result_addition)
+
+# Subtraction
+result_subtraction = 10 - 4
+print("Subtraction Result:", result_subtraction)
+
+# Multiplication
+result_multiplication = 2 * 6
+print("Multiplication Result:", result_multiplication)
+
+# Division
+result_division = 8 / 2
+print("Division Result:", result_division)
+
+# Modulus
+result_modulus = 10 % 3
+print("Modulus Result:", result_modulus)
+
+# Exponentiation
+result_exponentiation = 2 ** 3
+print("Exponentiation Result:", result_exponentiation)
+
+# Comparison Operators
+# Equal to
+print("Is 5 equal to 5?", 5 == 5)
+
+# Not equal to
+print("Is 5 not equal to 3?", 5 != 3)
+
+# Greater than
+print("Is 10 greater than 5?", 10 > 5)
+
+# Less than
+print("Is 3 less than 8?", 3 < 8)
+
+# Greater than or equal to
+print("Is 7 greater than or equal to 7?", 7 >= 7)
+
+# Less than or equal to
+print("Is 4 less than or equal to 4?", 4 <= 4)
