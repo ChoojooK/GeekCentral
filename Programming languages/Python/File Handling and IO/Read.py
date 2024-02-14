1348ecd49d4fd74080bda0738e0ae8cba08bc758
ROkelbow <kelbowow@gmail.com>
2024-02-15 02:40:11 +0330

    Create Read.py

---
+++ Programming languages/Python/File Handling and IO/Read.py
@@ -0,0 +1,11 @@
+# Open a file for reading
+file = open("example.txt", "r")
+
+# Read the entire contents of the file
+contents = file.read()
+
+# Close the file
+file.close()
+
+# Print the contents of the file
+print(contents)
