a8d674cb9e17176271ecd25928f116e197505068
ROkelbow <kelbowow@gmail.com>
2024-02-15 03:28:03 +0330

    Create BitwiseOperators.cpp

---
+++ Programming languages/C++/BitwiseOperators.cpp
@@ -0,0 +1,7 @@
+unsigned int x = 5, y = 3;
+unsigned int resultAnd = (x & y);  // Bitwise AND
+unsigned int resultOr = (x | y);  // Bitwise OR
+unsigned int resultXor = (x ^ y);  // Bitwise XOR
+unsigned int resultNotX = ~x;  // Bitwise NOT
+unsigned int resultLeftShift = (x << 1);  // Left shift
+unsigned int resultRightShift = (x >> 1);  // Right shift
