fc7385fbc496f2a459711ec0c5f0883395f2d9aa
ROkelbow <kelbowow@gmail.com>
2024-02-15 02:40:51 +0330

    Create Write.py

---
+++ Programming languages/Python/File Handling and IO/Write.py
@@ -0,0 +1,9 @@
+# Open a file for writing
+file = open("output.txt", "w")
+
+# Write data to the file
+file.write("Hello, world!\n")
+file.write("This is a test.\n")
+
+# Close the file
+file.close()
