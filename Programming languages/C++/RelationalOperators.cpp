e2efdd59c1f047911f5c84c912e2769cafbf480a
ROkelbow <kelbowow@gmail.com>
2024-02-15 03:27:02 +0330

    Create RelationalOperators.cpp

---
+++ Programming languages/C++/RelationalOperators.cpp
@@ -0,0 +1,7 @@
+int x = 10, y = 5;
+bool isEqual = (x == y);  // Equal to
+bool isNotEqual = (x != y);  // Not equal to
+bool isGreater = (x > y);  // Greater than
+bool isLess = (x < y);  // Less than
+bool isGreaterOrEqual = (x >= y);  // Greater than or equal to
+bool isLessOrEqual = (x <= y);  // Less than or equal to
