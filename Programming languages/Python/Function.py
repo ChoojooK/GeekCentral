6b99e58c92673e1eb13be8902f7fe0d9b3b31c11
ROkelbow <kelbowow@gmail.com>
2024-02-14 21:46:34 +0330

    Create Function.py

---
+++ Programming languages/Python/Function.py
@@ -0,0 +1,17 @@
+# Functions Python Script
+
+# Define a function to greet a person
+def greet(name):
+    return "Hello, " + name + "!"
+
+# Call the greet function with a specific name
+message = greet("Alice")
+print(message)
+
+# Define a function to calculate the area of a rectangle
+def calculate_rectangle_area(length, width):
+    return length * width
+
+# Call the calculate_rectangle_area function with specific length and width values
+rectangle_area = calculate_rectangle_area(5, 3)
+print("Area of the rectangle:", rectangle_area)
