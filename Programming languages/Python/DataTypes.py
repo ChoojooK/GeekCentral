041eaa94b62b28e1ec9e1b2cae6a555b7656cc3e
ROkelbow <kelbowow@gmail.com>
2024-02-14 21:40:23 +0330

    Create DataTypes.py

---
+++ Programming languages/Python/DataTypes.py
@@ -0,0 +1,17 @@
+# Assign values to variables (to ensure independence)
+name = "John"
+age = 30
+is_student = False
+pi = 3.14
+
+# Check variable types using type() function
+print("Type of 'name' variable:", type(name))
+print("Type of 'age' variable:", type(age))
+print("Type of 'is_student' variable:", type(is_student))
+print("Type of 'pi' variable:", type(pi))
+
+# Dynamic typing: Variables can hold different types of values
+dynamic_variable = "Hello"
+print("Dynamic Variable:", dynamic_variable)
+dynamic_variable = 123
+print("Updated Dynamic Variable:", dynamic_variable)
