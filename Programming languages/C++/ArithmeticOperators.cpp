4219bf6fcb480823501b2d6c89d9310f2434171a
ROkelbow <kelbowow@gmail.com>
2024-02-15 03:26:24 +0330

    Create ArithmeticOperators.cpp

---
+++ Programming languages/C++/ArithmeticOperators.cpp
@@ -0,0 +1,6 @@
+int a = 10, b = 5;
+int sum = a + b;  // Addition
+int difference = a - b;  // Subtraction
+int product = a * b;  // Multiplication
+int quotient = a / b;  // Division
+int remainder = a % b;  // Modulus
