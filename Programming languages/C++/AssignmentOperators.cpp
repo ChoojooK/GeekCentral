abd595a7caca00a7d5522545be16fdf533db26ed
ROkelbow <kelbowow@gmail.com>
2024-02-15 03:28:47 +0330

    Create AssignmentOperators.cpp

---
+++ Programming languages/C++/AssignmentOperators.cpp
@@ -0,0 +1,6 @@
+int x = 10;
+x += 5;  // Equivalent to x = x + 5
+x -= 3;  // Equivalent to x = x - 3
+x *= 2;  // Equivalent to x = x * 2
+x /= 4;  // Equivalent to x = x / 4
+x %= 3;  // Equivalent to x = x % 3
