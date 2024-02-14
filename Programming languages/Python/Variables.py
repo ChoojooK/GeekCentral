1ae16203d90bac262d01f7353930bd21b2c149d2
ROkelbow <kelbowow@gmail.com>
2024-02-14 21:37:30 +0330

    Create Variables.py

---
+++ Programming languages/Python/Variables.py
@@ -0,0 +1,15 @@
+# Assign values to variables
+name = "John"
+age = 30
+is_student = False
+pi = 3.14
+
+# Print variable values
+print("Name:", name)
+print("Age:", age)
+print("Is Student?", is_student)
+print("Value of Pi:", pi)
+
+# Variable reassignment
+age = 35
+print("Updated Age:", age)
