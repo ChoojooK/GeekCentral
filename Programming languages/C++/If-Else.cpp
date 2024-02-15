f0f944304da4f88f80f9218633d68255eed969e0
ROkelbow <kelbowow@gmail.com>
2024-02-15 03:31:04 +0330

    Create If-Else.cpp

---
+++ Programming languages/C++/If-Else.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main() {
+    int num = 10;
+
+    // Check if num is greater than 0
+    if (num > 0) {
+        std::cout << "The number is positive." << std::endl;
+    } else {
+        std::cout << "The number is non-positive." << std::endl;
+    }
+
+    return 0;
+}
