6bee50402a566bfc89d8e10f0cd12daef1c87110
ROkelbow <kelbowow@gmail.com>
2024-02-15 04:01:32 +0330

    Create DynamicMemoryAllocation(NewKeyword).cpp

---
+++ Programming languages/C++/DynamicMemoryAllocation(NewKeyword).cpp
@@ -0,0 +1,5 @@
+// Allocate memory for a single object
+int* ptr = new int; // Allocates memory for an integer
+
+// Allocate memory for an array of objects
+int* arr = new int[10]; // Allocates memory for an array of 10 integers
