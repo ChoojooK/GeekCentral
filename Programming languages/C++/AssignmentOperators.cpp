5b5a84f623af8bea8e731fb7aeefd7fa16eb6446
ROkelbow <kelbowow@gmail.com>
2024-09-10 01:31:50 +0330

    Update AssignmentOperators.cpp

--- Programming languages/C++/AssignmentOperators.cpp
+++ Programming languages/C++/AssignmentOperators.cpp
@@ -1,5 +1,7 @@
 // Assignment operators are used to assign values to variables
 
+// Compound assignment operators (like +=, -=) perform an operation and assignment in one step, making the code shorter and often easier to read.
+
 int x = 10;
 x += 5;  // Equivalent to x = x + 5
 x -= 3;  // Equivalent to x = x - 3
