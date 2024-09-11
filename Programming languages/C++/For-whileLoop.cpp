c5d432b98fadce7b3fb57df7a331e4e0db3e91bb
ROkelbow <kelbowow@gmail.com>
2024-09-11 10:33:02 +0330

    Update For-whileLoop.cpp

--- Programming languages/C++/For-whileLoop.cpp
+++ Programming languages/C++/For-whileLoop.cpp
@@ -1,3 +1,6 @@
+// The for loop is often used when you know in advance how many times you want to repeat a block of code.
+//It includes initialization, condition, and iteration expressions all in one line.
+
 #include <iostream>
 
 int main() {
