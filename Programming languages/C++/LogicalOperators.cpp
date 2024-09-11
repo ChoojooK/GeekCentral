cb68cc780a9a9ecc8108cdf84b004cae8e4bf866
ROkelbow <kelbowow@gmail.com>
2024-09-11 10:39:48 +0330

    Update LogicalOperators.cpp

--- Programming languages/C++/LogicalOperators.cpp
+++ Programming languages/C++/LogicalOperators.cpp
@@ -1,3 +1,8 @@
+/*
+Logical operators in C++ are used to combine or invert boolean expressions.
+They help in making complex decisions by evaluating multiple conditions.
+*/
+
 bool a = true, b = false;
 bool resultAnd = (a && b);  // Logical AND
 bool resultOr = (a || b);  // Logical OR
