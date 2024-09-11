5e76af606b3570c3d2393795f52671857aaa9993
ROkelbow <kelbowow@gmail.com>
2024-09-11 10:15:02 +0330

    Update BitwiseOperators.cpp

--- Programming languages/C++/BitwiseOperators.cpp
+++ Programming languages/C++/BitwiseOperators.cpp
@@ -1,3 +1,5 @@
+// Bitwise operators in C++ are used to perform operations on the binary representations of integers. They work directly on the bits of the numbers.
+
 unsigned int x = 5, y = 3;
 unsigned int resultAnd = (x & y);  // Bitwise AND
 unsigned int resultOr = (x | y);  // Bitwise OR
