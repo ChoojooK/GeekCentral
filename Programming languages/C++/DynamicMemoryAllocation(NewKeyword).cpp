3fb33ad9e27e8c4a1b263cad9cd5548615c982d1
ROkelbow <kelbowow@gmail.com>
2024-09-11 10:29:20 +0330

    Update DynamicMemoryAllocation(NewKeyword).cpp

--- Programming languages/C++/DynamicMemoryAllocation(NewKeyword).cpp
+++ Programming languages/C++/DynamicMemoryAllocation(NewKeyword).cpp
@@ -1,3 +1,9 @@
+/*
+In C++, dynamic memory allocation allows you to allocate and manage memory at runtime, rather than at compile time.
+This is useful for creating data structures whose size is not known until the program is running.
+The new keyword is used to allocate memory dynamically.
+*/
+
 #include <iostream>
 
 int main() {
