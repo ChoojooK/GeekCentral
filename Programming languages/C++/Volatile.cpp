928e675367b43938f4ec867d5982c68cda61b961
ROkelbow <kelbowow@gmail.com>
2024-09-11 10:54:25 +0330

    Update Volatile.cpp

--- Programming languages/C++/Volatile.cpp
+++ Programming languages/C++/Volatile.cpp
@@ -1,3 +1,8 @@
+/*
+In C++, the volatile keyword is used to indicate that a variable's value may be changed by external factors that the compiler cannot predict, such as hardware registers or concurrent threads.
+This keyword tells the compiler not to optimize code involving that variable, ensuring that every access to it reflects its most current value.
+*/
+
 #include <iostream>
 
 int main() {
