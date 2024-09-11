36c9e0ac3bc0fb21ef397522547f3ab827acc43b
ROkelbow <kelbowow@gmail.com>
2024-09-11 10:42:58 +0330

    Update Mutable.cpp

--- Programming languages/C++/Mutable.cpp
+++ Programming languages/C++/Mutable.cpp
@@ -1,3 +1,9 @@
+/*
+In C++, the mutable keyword is used to allow a particular data member of a class to be modified even if the object is declared as const.
+By default, all member variables of a const object are also const, meaning they cannot be modified.
+The mutable keyword provides an exception to this rule.
+*/
+
 class MyClass {
 private:
     mutable int cacheValue; // Mutable member variable
