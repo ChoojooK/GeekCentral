29a65154e673e63dbde3393706cbfcb690984edf
ROkelbow <kelbowow@gmail.com>
2024-09-11 10:37:35 +0330

    Update Increment-Decrement-Operators.cpp

--- Programming languages/C++/Increment-Decrement-Operators.cpp
+++ Programming languages/C++/Increment-Decrement-Operators.cpp
@@ -1,3 +1,8 @@
+/*
+In C++, increment and decrement operators are used to increase or decrease the value of a variable by one.
+They are shorthand notations that make your code more concise and readable.
+*/
+
 int x = 5;
 x++;  // Increment x by 1
 x--;  // Decrement x by 1
